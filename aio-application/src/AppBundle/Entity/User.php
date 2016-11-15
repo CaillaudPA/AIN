30c5029206a7248a4b78fc00545e07958cb1c950
CaillaudPA <caillaud.pierreantoine@yahoo.com>
2016-11-15 09:12:08 +0100

    add user$

---
+++ aio-application/src/AppBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pierre-Antoine
+ * Date: 15/11/2016
+ * Time: 08:59
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
