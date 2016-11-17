6b399e70217ba92f7c5a1906e8487cd0ccc6443d
Unknown <robin.cornec@epsi.fr>
2016-11-17 14:30:17 +0100

    twitter début

--- aio-application/src/AppBundle/Controller/TwitterController.php
+++ aio-application/src/AppBundle/Controller/TwitterController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Service\TwitterService;
+use Endroid\Twitter\Twitter;
 
 class TwitterController extends Controller
 {
@@ -16,7 +17,19 @@ class TwitterController extends Controller
      */
     public function indexAction()
     {
-        // ...
+        $consumerKey = $this->getParameter('twitter.consumer_key');
+        $consumerSecret = $this->getParameter('twitter.consumer_secret');
+        $accessToken = $this->getParameter('twitter.access_token');
+        $accessTokenSecret = $this->getParameter('twitter.access_secret');
+
+        $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+        $response = $twitter->query('oauth/authenticate', 'GET', 'json', array(
+            //"oauth_callback" => "http://127.0.0.1/aio/aio-application/web/app_dev.php/twitter_connect/callback"
+        ));
+
+        //$response = $twitter->query('statuses/user_timeline', 'GET', 'json');
+        dump($response);die;
     }
 
 }
