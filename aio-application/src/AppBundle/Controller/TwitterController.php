c94c7c747f1ef217fd2c3c5c2b99c2750c870637
Unknown <robin.cornec@epsi.fr>
2016-11-17 08:30:33 +0100

    i don't know

--- aio-application/src/AppBundle/Controller/TwitterController.php
+++ aio-application/src/AppBundle/Controller/TwitterController.php
@@ -4,7 +4,19 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Service\TwitterService;
 
 class TwitterController extends Controller
 {
+
+    /**
+     * @Route("/")
+     *
+     * Just for example
+     */
+    public function indexAction()
+    {
+        // ...
+    }
+
 }
